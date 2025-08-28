eac6d0ff0ddd5724e7d02f609b0bff4339d85e9c
mitva-panchal-2005 <mittupanchal842@gmail.com>
2025-08-28 13:38:15 +0530

    Add files via upload

---
+++ python1.assignment/practical/pra11 (1).py
@@ -0,0 +1,10 @@
+my_dict = {
+    "name": "dhruvi",
+    "age": 19,
+    "city": "ahemadabad",
+    "is_student": True,
+    "height": 5.3,
+    "hobbies": ["cooking", "baking", "music"]
+}
+
+print("Dictionary:", my_dict)
---
+++ python1.assignment/practical/pra13 (1).py
@@ -0,0 +1,11 @@
+my_dict = {
+    "name": "Dhruvi",
+    "age": 19,
+    "city": "Radhanpur"
+}
+
+print("Original dictionary:", my_dict)
+
+my_dict["city"] = "ahemadabad"
+
+print("Updated dictionary:", my_dict)
